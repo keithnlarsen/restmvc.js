8be90105cf477fdf541805471a1171c44936b49d
keithnlarsen <keithnlarsen@gmail.com>
2011-05-25 11:37:51 -0600

    Fixed error handling bugs

--- example/models/person.js
+++ example/models/person.js
@@ -1,7 +1,7 @@
 module.exports.person = function (mongoose) {
     // Standard Mongoose stuff here...
     var schema = mongoose.Schema;
-    var objectId = schema.ObjectId;
+//    var objectId = schema.ObjectId;
 
     mongoose.model('Person', new schema({
 //        _id: objectId,
